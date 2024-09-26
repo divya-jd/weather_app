f5cbc26901156bcc1323dd9cbfdb1d3400217a3b
divya-jd <gorla.joise@gmail.com>
2024-09-25 20:38:26 -0400

    Basic UI with input field and button

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,36 @@ class SimpleWeatherApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Simple Weather Info App'),
         ),
-        body: Center(
-          child: Text('Welcome to the Weather Info App!'),
-        ),
+        body: WeatherUI(),
+      ),
+    );
+  }
+}
+
+class WeatherUI extends StatelessWidget {
+  final TextEditingController _cityController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller: _cityController,
+            decoration: InputDecoration(labelText: 'Enter City Name'),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Fetch Weather'),
+          ),
+          SizedBox(height: 20),
+          Text('City: --'),
+          Text('Temperature: --'),
+          Text('Condition: --'),
+        ],
       ),
     );
   }
